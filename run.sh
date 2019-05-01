1456c67e7a2181e12554e0d5040c7318418cfa62
Mahika Dubey <mahika@Mahikas-MacBook-Air.local>
2019-05-01 01:12:35 -0700

    updated coco address

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ export PYTHONUNBUFFERED=0
 python style.py --style images/bruises_giorgialupi.jpg \
   --checkpoint-dir checkpoints/ \
   --vgg-path /styletransfer/data/imagenet-vgg-verydeep-19.mat \
-  --train-path /datasets/coco/ \
+  --train-path /datasets/coco/coco_train2014 \
   --model-dir /artifacts/ \
   --test images/violetaparra.jpg \
   --test-dir tests/ \
