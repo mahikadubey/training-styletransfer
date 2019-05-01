ce2312f6df5623e1af86d88fe62a4ed779cdbc2c
Mahika Dubey <mahika@Mahikas-MacBook-Air.local>
2019-04-30 23:28:15 -0700

    train path

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 export PYTHONUNBUFFERED=0
 python style.py --style images/bruises_giorgialupi.jpg \
   --checkpoint-dir checkpoints/ \
+  --vgg-path /data/imagenet-vgg-verydeep-19.mat \
+  --train-path /datasets/coco/ \
   --model-dir models/ \
   --test images/violetaparra.jpg \
   --test-dir tests/ \
