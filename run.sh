57e5a460a04039c078680be81807999395ca15fe
Mahika Dubey <mahika@eduroam-169-233-163-156.ucsc.edu>
2019-06-06 19:30:58 -0700

    thorpe model

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 export PYTHONUNBUFFERED=0
-python style.py --style images/minard-cattle-zoom.png \
+python style.py --style images/thorpe.jpg \
   --checkpoint-dir checkpoints/ \
   --vgg-path /styletransfer/data/imagenet-vgg-verydeep-19.mat \
   --train-path /datasets/coco/coco_train2014 \
